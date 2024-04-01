e8c7a53f03f751f55c1f9e25b4c6e62a12b5dd26
unknown <wonew123@naver.com>
2024-04-01 15:20:23 +0900

    은행계좌 클래스 제작

---
+++ Account.java
@@ -0,0 +1,34 @@
+package exceptionEx;  //inheritance:상속  //상속패키지에서 exception 패키지로 복사해옴
+//은행계좌클래스(국민은행)
+public class Account {
+	//멤버변수(필드)
+	String accountNo;  //계좌번호
+	String ownerName;  //예금주이름
+	int balance;  //잔액
+	
+	//생성자  => ctrl+space 누르면 빈클래스가 나옴.
+	public Account() {
+		
+	}
+
+	public Account(String accountNo, String ownerName, int balance) {
+		this.accountNo = accountNo;
+		this.ownerName = ownerName;
+		this.balance = balance;
+	}
+	
+	//예금한다 메서드
+	void deposit(int amount) {
+		balance+=amount;
+	}
+	
+	//인출한다 메서드
+	int withdraw(int amount) throws InsufficientException {
+		if(balance < amount) {
+			throw new InsufficientException("잔고 부족("+(amount-balance)+"원 모자람)"); 
+		}
+		balance-=amount;
+		return amount;
+	}
+
+}
